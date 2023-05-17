43283eb57fb584e340b57f590cf0a4a81e34fd93
Léa Yeromonahos <leayero@gmail.com>
2023-05-17 14:25:17 +0700

    hide personnal path

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import csvUtils as CSV
 
 if __name__ == "__main__":
 
-    normalised_timestamp_acc, x_accel, y_accel, z_accel = CSV.reading_into_csv("C:/Users/leaye/Documents/Lea/Etudes/SeaTech/2A/Stage/prog/data/RGB/Subject001/03/09.csv", 1, 2, 3)
-    normalised_timestamp_gyr, x_gyr, y_gyr, z_gyr = CSV.reading_into_csv("C:/Users/leaye/Documents/Lea/Etudes/SeaTech/2A/Stage/prog/data/RGB/Subject001/03/09.csv", 4, 5, 6)
+    normalised_timestamp_acc, x_accel, y_accel, z_accel = CSV.reading_into_csv("data/RGB/Subject001/03/09.csv", 1, 2, 3)
+    normalised_timestamp_gyr, x_gyr, y_gyr, z_gyr = CSV.reading_into_csv("data/RGB/Subject001/03/09.csv", 4, 5, 6)
 
 
     plt.figure(1)
