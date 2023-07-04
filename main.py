bb61f9dd588c481cc41a285d5f0d2409dc970293
Léa Yeromonahos <leayero@gmail.com>
2023-07-04 18:01:29 +0700

    added our datas

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import readingsUtils.csv_reading.txtUtils as TXT
 import readingsUtils.csv_reading.csvUtils as CSV
-import matplotlib.pyplot as plt
 import numpy as np
 import signalUtils as SIGNAL
-import graphUtils as GRAPH
-from math import sqrt
 
 if __name__ == "__main__":
 
         normalised_timestamp_acc, x_gyros, y_gyros, z_gyros, x_accel, y_accel, z_accel = CSV.reading_into_csv(
-                "data/our_datas/non_gesture/Non-gesture_8_Lea.csv")
+                "data/our_datas/gestures/processed/seq9.csv")
 
         # PARAMETERS FOR THE REST OF THE CODE
         window_size = 5  # Size of the moving window for computing mean and standard deviation
         envelopp_multiplier = 3  # Multiplier for the standard deviation to determine the envelopp
         threshold_multiplier = 0.5 # if too low (<0.4), detection check shows ungesture instead of gesture, if too high, gesture are too much segmented
 
-        sigma = 0.1
+        sigma = 2
 
 
         # SEGMENTATION
