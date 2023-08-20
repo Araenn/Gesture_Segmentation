fix precision, iou

mathsUtils.py
@@ -1,7 +1,9 @@
 from scipy.ndimage import gaussian_filter1d
-from typing import List
-from math import sqrt, pow, atan, pi
-import numpy as np
+from typing import List, Tuple
+from math import sqrt, pow
+import readingUtils.csvUtils as CSV
+import graphUtils as GRAPH
+
 
 def derivative(normalised_timestamp_acc, x_accel):
     x_derivative = []
@@ -91,6 +93,73 @@ def non_max_suppression(seg_start_list, seg_end_list, threshold):
            
     return selected_indices_start, selected_indices_end
 
+def is_supperposed(start_a:float, end_a:float, start_b:float, end_b:float) -> bool:
+    Xa = max(start_a, start_b)
+    Xb = min(end_a, end_b)
+    return Xb - Xa > 0
+
+from enum import Enum, auto
+
+class DetectionType(Enum):
+    NONE = auto()
+    TRUE_POS = auto()
+    TRUE_NEG = auto()
+    FALSE_POS = auto()
+    FALSE_NEG = auto()
+
+def determine_detection(reals_start, dets_start, reals_end, dets_end) -> Tuple[List[Tuple[DetectionType, int]]]:
+    assert len(reals_start) == len(reals_end)
+    assert len(dets_start) == len(dets_end)
+
+    real_n = len(reals_start)
+    det_n = len(dets_start)
+    reals_type = [DetectionType.NONE] * real_n
+    dets_type = [DetectionType.NONE] * det_n
+
+    for i_real in range(real_n):
+        real_start = reals_start[i_real]
+        real_end = reals_end[i_real]
+        found = False
+        was_count = False
+        for i_det in range(det_n):
+            det_start = dets_start[i_det]
+            det_end = dets_end[i_det]
+
+            if is_supperposed(real_start, real_end, det_start, det_end) and not was_count:
+                #print(f"{i_real} est supperposé à {i_det}")
+                reals_type[i_real] = (DetectionType.TRUE_POS, i_det)
+                dets_type[i_det] = (DetectionType.TRUE_POS, i_real)
+                found = True
+                was_count = True
+            elif is_supperposed(real_start, real_end, det_start, det_end) and was_count:
+                dets_type[i_det] = (DetectionType.FALSE_POS, i_real)
+                found = True
+                was_count = True
+
+        if not found:
+            reals_type[i_real] = (DetectionType.FALSE_NEG, -1)
+    
+    for i_det in range(det_n):
+        if dets_type[i_det] == DetectionType.NONE:
+            dets_type[i_det] = (DetectionType.FALSE_POS, -1)
+
+    return reals_type, dets_type
+
+def precision_recall(reals_start, dets_start, reals_end, dets_end):
+    _, dets_type = determine_detection(reals_start, dets_start, reals_end, dets_end)
+    TP = 0
+    FP = 0
+    FN = 0
+    for i in range(len(dets_type)):
+        if dets_type[i][0] == DetectionType.TRUE_POS:
+            TP += 1
+        elif dets_type[i][0] == DetectionType.FALSE_NEG:
+            FN += 1
+        elif dets_type[i][0] == DetectionType.FALSE_POS:
+            FP += 1
+
+    precision = TP / (TP + FP)
+    return precision
 
 def IOU(start_true, end_true, start_detected, end_detected):
     Xa = max(start_true, start_detected)
@@ -100,129 +169,32 @@ def IOU(start_true, end_true, start_detected, end_detected):
     res = inter / union
     return res
 
-def true_false_positive_negative(iou:float, is_gesture:bool):
-    threshold = 0.5
 
-    true_positiv = 0
-    false_positiv = 0
-    true_negativ = 0
-    false_negativ = 0
+def check_iou(reals_start, dets_start, reals_end, dets_end, num):
+    assert len(reals_start) == len(reals_end)
+    assert len(dets_start) == len(dets_end)
+
+    reals_type, dets_type = determine_detection(reals_start, dets_start, reals_end, dets_end)
+    n_reals = len(reals_start)
+
+    iou = []
     
-    if iou > threshold and is_gesture:
-        true_positiv += 1
-    elif iou > threshold and not is_gesture:
-        false_positiv += 1
-    elif iou < threshold and is_gesture:
-        false_negativ += 1
-    elif iou > threshold and not is_gesture:
-        true_negativ += 1
+    for i_real in range(n_reals):
+        detection_type, i_det = reals_type[i_real]
+        if detection_type == DetectionType.TRUE_POS:
+            iou.append(IOU(reals_start[i_real], 
+                           reals_end[i_real],
+                           dets_start[i_det],
+                           dets_end[i_det]))
 
-    return true_positiv, true_negativ, false_positiv, false_negativ
 
-def mean(data):
-    return sum(data)/len(data)
-
-def build_angles(accs_x, accs_y, accs_z, gyrs_x, gyrs_y, gyrs_z, timestamps):
-    n = len(timestamps)
-    acc_angles_x = [0] * n
-    acc_angles_y = [0] * n
-    for i in range(len(timestamps)):
-        acc_angles_x[i] = atan(accs_y[i] / sqrt(pow(accs_x[i], 2) + pow(accs_z[i], 2))) * 180 / pi
-        acc_angles_y[i] = atan(-1 * accs_x[i] / sqrt(pow(accs_y[i], 2) + pow(accs_z[i], 2))) * 180 / pi
-
-    gyro_angles_x = [0] * n
-    gyro_angles_y = [0] * n
-    dt = timestamps[1] - timestamps[0]
-    current_gyro_angle_x = 0.0
-    current_gyro_angle_y = 0.0
-    current_gyro_angle_x = gyrs_x[0] * dt
-    current_gyro_angle_y = gyrs_y[0] * dt
-    gyro_angles_x[0] = current_gyro_angle_x
-    gyro_angles_y[0] = current_gyro_angle_y
-
-    yaws = [0] * n
-    rolls = [0] * n
-    pitchs = [0] * n
-    current_yaw = 0
-    current_yaw = gyrs_z[0] + dt
-    yaws[0] = current_yaw
-
-    total_time = 0
-    ERROR_YAW = 0
-    ERROR_ROLL = 0
-    ERROR_PITCH = 0
-    for i in range(1, n):
-        dt = timestamps[i] - timestamps[i - 1]
-        total_time += dt
-        current_gyro_angle_x += gyrs_x[i] * dt
-        current_gyro_angle_y += gyrs_y[i] * dt
-        gyro_angles_x[i] = current_gyro_angle_x
-        gyro_angles_y[i] = current_gyro_angle_y
-
-        current_yaw += gyrs_z[i] * dt
-        yaws[i] = current_yaw
-        rolls[i] = gyro_angles_x[i]
-        pitchs[i] = gyro_angles_y[i]
-        
-    ERROR_YAW = yaws[-1] / total_time
-    ERROR_ROLL = rolls[-1] / total_time
-    ERROR_PITCH = pitchs[-1] / total_time
-
-    total_time = 0
-    current_yaw = 0
-    current_yaw = gyrs_z[0] + dt
-    for i in range(1, n):
-        dt = timestamps[i] - timestamps[i - 1]
-        total_time += dt
-        current_gyro_angle_x += gyrs_x[i] * dt
-        current_gyro_angle_y += gyrs_y[i] * dt
-        gyro_angles_x[i] = current_gyro_angle_x
-        gyro_angles_y[i] = current_gyro_angle_y
-
-        yaws[i] -= ERROR_YAW * total_time
-        rolls[i] -= ERROR_ROLL * total_time
-        pitchs[i] -= ERROR_PITCH * total_time
-    return rolls, pitchs, yaws
-
-def init_norms(timestamps, accs_x, accs_y, accs_z):
-        norms = []
-        n = len(timestamps)
-        for i in range(n):
-            norms.append(sqrt(accs_x[i] * accs_x[i] + accs_y[i] * accs_y[i] + accs_z[i] * accs_z[i]))
-        return norms
-
-def compensate_gravity(timestamps, accs_x, accs_y, accs_z, gyrs_x, gyrs_y, gyrs_z):
-    alpha = 0.6
-    rolls, pitchs, yaws = build_angles(accs_x, accs_y, accs_z, gyrs_x, gyrs_y, gyrs_z, timestamps)
-    for i in range(len(timestamps)):
-        # Intégration des valeurs du gyroscope pour estimer l'angle d'inclinaison
-        roll = rolls[i] / 180 * pi
-        pitch = pitchs[i] / 180 * pi
-        yaw = yaws[i] / 180 * pi
-
-        # Projection de l'accélération sur les axes du référentiel du capteur
-        # selon l'angle d'inclinaison estimé
-        R_x = np.array([[1, 0, 0],
-                        [0, np.cos(roll), -np.sin(roll)],
-                        [0, np.sin(roll), np.cos(roll)]])
-        R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-                        [0, 1, 0],
-                        [-np.sin(pitch), 0, np.cos(pitch)]])
-        R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-                    [np.sin(yaw), np.cos(yaw), 0],
-                    [0, 0, 1]])
-
-        accel = np.array([accs_x[i], accs_y[i], accs_z[i]]).reshape(-1, 1)
-        projected_accel = np.dot(R_z, np.dot(R_y, np.dot(R_x, accel)))
-
-        # Compensation de la gravité
-        compensated_accel = projected_accel - np.array([0, 0, 1]).reshape(-1, 1)
-
-        # Filtre complémentaire pour combiner l'accélération compensée et non compensée
-        compensated_accel = alpha * compensated_accel + (1 - alpha) * np.array([accs_x[i], accs_y[i], accs_z[i]]).reshape(-1, 1)
-
-        accs_x[i] = float(compensated_accel[0][0])
-        accs_y[i] = float(compensated_accel[1][0])
-        accs_z[i] = float(compensated_accel[2][0])
-
-    return accs_x, accs_y, accs_z
+    print(f"iou : {iou}")
+    CSV.write_to_csv(iou, f"results/iou{num}.csv")
+    GRAPH.plots_data([x for x in range(len(iou))], 
+                     f"IOU {num}", 
+                     False, 
+                     (iou, "iou"), 
+                     ([0 for i in range(len(iou))], "0"), 
+                     ([1 for i in range(len(iou))], "1")
+                    )
+    return iou